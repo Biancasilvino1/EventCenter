09ea9de17c77dda640380aa6b150c1ef3f4bd192
RomuloRallye <39503363+RomuloRallye@users.noreply.github.com>
2018-06-28 23:06:02 -0300

    Add files via upload

---
+++ EventCenter/proc_edit_usuario.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include_once("conexao.php");
+
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_STRING);
+$telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING);
+$endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+//echo "Nome: $nome <br>";
+//echo "E-mail: $email <br>";
+
+$result_usuario = "UPDATE clientes SET nome='$nome', cpf='$cpf', telefone='$telefone', endereco='$endereco', 
+email='$email', usuario='$usuario', senha='$senha' WHERE id='$id'";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if(mysqli_affected_rows($conn)){
+	$_SESSION['msg'] = "<p style='color:green;'>Usuário editado com sucesso</p>";
+	header("Location: listar.php");
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi editado com sucesso</p>";
+	header("Location: editar.php?id=$id");
+}
